6b9e258c9249b181cedb84b8b43cce73f709b3ad
Tamyres Lopes <112120038+tamyreslfs@users.noreply.github.com>
2022-11-14 23:04:19 -0300

    Add files via upload

---
+++ transicao.js
@@ -0,0 +1,11 @@
+ScrollReveal({ reset: true });
+/*Banner do site*/
+ScrollReveal().reveal( '.poster',{delay:500, duration:1500} );
+/*Minha foto com nome e curriculo*/
+ScrollReveal().reveal( '.fotonome',{delay:500, duration:2000} );
+/*Sobre mim*/
+ScrollReveal().reveal( '.sobremim',{delay:500, duration:3000} );
+/*Formulario para contato*/
+ScrollReveal().reveal( '.formulario',{delay:500, duration:4000} );
+/*Contato via redes sociais*/
+ScrollReveal().reveal( '.contato',{delay:500, duration:5000} );
